1d90373bac265e8492570f80cfdd7e426b7399b5
lhl09120 <lhl09120@gmail.com>
2018-04-08 17:20:22 +0800

    feat：project init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vue-autoload-script-style-loader
-a loader to auto load script tag and style tag in .vue file
+### 介绍
+
+当.vue文件里面不写`<script>`或者`<style>`标签时，能够自动检测并且装载js文件或css(less/sass)文件到.vue文件中
+
+### 注意事项
+
+该loader必须在vue-loader之前执行
+
+### 未来版本功能设计
+
+#### v0.1.3
+1. 支持配置js和css的文件目录，不限于同目录下
+2. 支持根据文件名规则进行自动装载，不限于同名文件
+
+### 已发布版本功能概览
+
+#### v0.1.2
+1. 添加README.md文件
+
+#### v0.1.1
+1. 初始版本，当.vue文件里面不写`<script>`或者`<style>`标签时，能够自动检测并且装载同目录下同名的js文件或css(less/sass)文件到.vue文件中
---
+++ index.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+
+function getFilePathByRequest(request) {
+  return request.split('vue-auto-loader/index.js!')[1];
+}
+
+function getFileNameByPath(path) {
+  return path.split('/').reverse()[0];
+}
+
+function hasScriptTag(fileSource) {
+  return /<script/.test(fileSource) && /<\/script>/.test(fileSource);
+}
+
+function hasStyleTag(fileSource) {
+  return /<style/.test(fileSource) && /<\/style>/.test(fileSource);
+}
+
+function addScriptTagToSource(fileSource, filePath, fileName) {
+  if (fs.existsSync(filePath.replace('\.vue', '.js'))) {
+    return fileSource + '<script src="./' + fileName.replace('\.vue', '.js') + '"></script>';
+  }
+  return fileSource;
+}
+
+function addStyleTagToSource(fileSource, filePath, fileName) {
+  if (fs.existsSync(filePath.replace('\.vue', '.less'))) {
+    return fileSource + '<style rel="stylesheet" lang="less">@import "./' + fileName.replace('\.vue', '.less') +'";</style>';
+  }
+  if (fs.existsSync(filePath.replace('\.vue', '.sass'))) {
+    return fileSource + '<style rel="stylesheet" lang="sass">@import "./' + fileName.replace('\.vue', '.sass') +'";</style>';
+  }
+  if (fs.existsSync(filePath.replace('\.vue', '.css'))) {
+    return fileSource + '<style rel="stylesheet">@import "./' + fileName.replace('\.vue', '.css') + '";</style>';
+  }
+  return fileSource;
+}
+
+module.exports = function(source) {
+  var filePath = getFilePathByRequest(this.request);
+  var fileName = getFileNameByPath(filePath);
+  if (!hasScriptTag(source)) {
+    source = addScriptTagToSource(source, filePath, fileName);
+  }
+  if (!hasStyleTag(source)) {
+    source = addStyleTagToSource(source, filePath, fileName);
+  }
+  return source;
+}
